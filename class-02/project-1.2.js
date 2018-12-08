c5a6ce369204b2822613d763f66ab9f63815c7ae
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-08 13:12:43 +0600

    number
    how to use number in javascript.

---
+++ class-02/project-1.2.js
@@ -0,0 +1,144 @@
+console.log(125)
+125
+undefined
+> console.log(125+125)
+250
+undefined
+> console.log(125*25)
+3125
+undefined
+> console.log(125/25)
+5
+undefined
+> console.log(125-25)
+100
+undefined
+> console.log(125+25-25/5*6)
+120
+undefined
+> console.log(125.112)
+125.112
+undefined
+> console.log(3.14)
+3.14
+undefined
+> console.log(422)
+422
+undefined
+> console.log(420)
+420
+undefined
+> console.log(10)
+10
+undefined
+> console.log(0b10)
+2
+undefined
+> console.log(0b11)
+3
+undefined
+> console.log(0b12)
+console.log(0b12)
+            ^^^
+
+SyntaxError: Invalid or unexpected token
+
+> console.log(0o10)
+8
+undefined
+> console.log(0o1)
+1
+undefined
+> console.loc(0o9)
+console.loc(0o9)
+            ^^
+
+SyntaxError: Invalid or unexpected token
+
+> console.log(0o12)
+10
+undefined
+> console.log(0xf)
+15
+undefined
+> console.log(0o9)
+console.log(0o9)
+            ^^
+
+SyntaxError: Invalid or unexpected token
+
+> console.log(Number.MAX_VALUE)
+1.7976931348623157e+308
+undefined
+> console.log(Number.MIN_VALUE)
+5e-324
+undefined
+> console.log(3e8)
+300000000
+undefined
+> console.log(Number.parse.Int("125"))
+TypeError: Cannot read property 'Int' of undefined
+> console.log(Number.parseInt("125"))
+125
+undefined
+> console.log(Number.parseInt("125.125"))
+125
+undefined
+> console.log("125")
+125
+undefined
+> console.log(Number.parseFloat("125.125"))
+125.125
+undefined
+> console.log(Number.parseFloat("125.12"))
+125.12
+undefined
+> console.log(infinity)
+ReferenceError: infinity is not defined
+> console.log(NaN)
+NaN
+undefined
+> console.log(typeof 244)
+number
+undefined
+> console.log(typeof "father")
+string
+undefined
+> console.log(typeof "125")
+string
+undefined
+> console.log(typeof NaN)
+number
+undefined
+> console.log(typeof Infinitu)
+undefined
+undefined
+> console.log(typeof Infinity)
+number
+undefined
+> console.log(125+25)
+150
+undefined
+> console.log(125.12+125
+... consol.log
+console.log(125.12+125
+                   ^^^
+
+SyntaxError: missing ) after argument list
+
+> console.log(125.12+25-25*23/5)
+35.120000000000005
+undefined
+> console.log(125%25)
+0
+undefined
+> console.log(125%5)
+0
+undefined
+> console.log(100.77%45)
+10.769999999999996
+undefined
+> console.log(100.25%35)
+30.25
+undefined
+>
