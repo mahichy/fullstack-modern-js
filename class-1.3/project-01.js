9e7fbaa4cfd6927f9323c8bf06a830788ba0079c
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-08 22:46:46 +0600

    Number js

--- class-1.3/project-01.js
+++ class-1.3/project-01.js
@@ -6,13 +6,5 @@ console.log(x+y);
 
 
 
-PS C:\Users\Mahi Chowdhury\Documents\GitHub\fullstack-modern-js\class-1.3> node project-01.js 100 50
-150
-PS C:\Users\Mahi Chowdhury\Documents\GitHub\fullstack-modern-js\class-1.3> node project-01.js 50 50
-100
-PS C:\Users\Mahi Chowdhury\Documents\GitHub\fullstack-modern-js\class-1.3> node project-01.js 120 80
-200
-PS C:\Users\Mahi Chowdhury\Documents\GitHub\fullstack-modern-js\class-1.3> node project-01.js 80 85
-165
-PS C:\Users\Mahi Chowdhury\Documents\GitHub\fullstack-modern-js\class-1.3>
+
 
