dd048b00261318eaae1d11a02db270aeacc0970d
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-08 22:11:06 +0600

    number js
    class 1.3
    how to use (+) in js
    project 01

--- class-1.3/project-01.js
+++ class-1.3/project-01.js
@@ -1,3 +1,18 @@
 let x = parseInt(process.argv[2]);
 let y = parseInt(process.argv[3]);
 console.log(x+y);
+
+
+
+
+
+PS C:\Users\Mahi Chowdhury\Documents\GitHub\fullstack-modern-js\class-1.3> node project-01.js 100 50
+150
+PS C:\Users\Mahi Chowdhury\Documents\GitHub\fullstack-modern-js\class-1.3> node project-01.js 50 50
+100
+PS C:\Users\Mahi Chowdhury\Documents\GitHub\fullstack-modern-js\class-1.3> node project-01.js 120 80
+200
+PS C:\Users\Mahi Chowdhury\Documents\GitHub\fullstack-modern-js\class-1.3> node project-01.js 80 85
+165
+PS C:\Users\Mahi Chowdhury\Documents\GitHub\fullstack-modern-js\class-1.3>
+
