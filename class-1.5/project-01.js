9ff1c84591b38af5998d1393f28c50a92d0dd6dd
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-09 16:22:34 +0600

    string part 01
    how to use "",'',and special sine in js

---
+++ class-1.5/project-01.js
@@ -0,0 +1,25 @@
+
+var a = "let's have fun";
+var b= 55;
+var c = `mahi
+bishal ${b}
+munir`;
+
+var d = "mahi"+"oshim";
+var e = "hello world"
+
+
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+
+
+
+
+
+
