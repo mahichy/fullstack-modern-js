3ac7c14d53a94b9cd5d4dca57c20f0cae30963c1
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-09 10:37:36 +0600

    variable
    how to use variable in js

---
+++ class-1.4/project-01.js
@@ -0,0 +1,21 @@
+var a = 1971;
+var b = 1982;
+let c =parseInt(process.argv[2]);
+let d = parseInt(process.argv[3]);
+
+
+console.log(a);
+console.log(a);
+console.log(a+48);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(c+d);
+
+
+
+
+
+
+
