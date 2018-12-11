682cc8c8aaa6bd5bb38ba8baf75e2a713e60c9c4
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-11 22:37:59 +0600

    string part 03
    here i know how to quary a ward

---
+++ class-1.7/project-01.js
@@ -0,0 +1,19 @@
+var a = "HELLO WORLD";
+var b = a.charAt(0);
+var c = a.charCodeAt(1);
+var f = a.indexOf('D');
+var g = a.match('HE');
+var h = a.replace('H','G');
+var i = a.length;
+var k = a.toUpperCase();
+var l = a.toLowerCase();
+
+var m = (`hello`===`hello`);
+
+
+var n =(` Dav Dass `);
+var o = n.trim().toLowerCase();
+
+
+console.log(b,c,f,g,h,a,i,k,l,m,n,o);
+
