e7a79ddd7d1466746756a05209e2207b02376fb8
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-12 23:05:53 +0600

    Boolean 1.9
    here i know how to use boolean

---
+++ class-1.9/Boolean.js
@@ -0,0 +1,28 @@
+var villain = false;
+var hero = true;
+
+var a = true;
+var b = false;
+var c = Boolean(345);
+var d = Boolean(true);
+var e = Boolean(false);
+var f = Boolean(0);
+var g = Boolean(+345);
+var h = Boolean(-345);
+var i = Boolean({});
+var j = Boolean([]);
+var k = Boolean(undefined);
+var l = Boolean("mahi");
+var m = Boolean("");
+var n = Boolean(NaN);
+var o = Boolean(null);
+
+
+
+
+
+
+
+
+
+console.log(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o);
