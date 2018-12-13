442b54b23d3b39b107ec48c7d8da916ad588adf7
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-13 20:36:06 +0600

    Create basic-control-statement.js
    Hear i know how to use if,else and elseif.

---
+++ class-1.11/basic-control-statement.js
@@ -0,0 +1,133 @@
+if (true){
+  console.log("yes");
+}
+else{
+  console.log("no");
+}
+if (false){
+  console.log("yes");
+  
+}
+else
+  console.log("no");
+
+var age =18;
+console.log("age"==="age")
+
+if (age>18){
+console.log("biyer boyosh hoyeche");
+}
+else
+{
+  console.log("biyer boyosh hoyni")
+}
+
+var age2 =18;
+if(age>=18){
+console.log("biyer boyosh hoyeche")
+}
+else
+{
+  console.log("biyer boyosh hoyni")
+}
+
+var age3 =18;
+if(age<=18){
+console.log("biyer boyosh hoyni")
+}
+else
+{
+  console.log("biyer boyosh hoyeche")
+}
+
+var age4 =18;
+if(age<18){
+console.log("biyer boyosh hoyni")
+}
+else
+{
+  console.log("biyer boyosh hoyeche")
+}
+
+if(false)
+{
+  console.log("hello world")
+}
+else
+{
+  console.log("not hellow world");
+}
+
+if(true)
+{
+  console.log("hellow world")
+}
+
+var age = 17;
+if (age>=21)
+{
+  console.log("Chele hole biyer boyosh hoyeche");
+}
+else if(age>=18)
+{
+  console.log("maya hole biyer boyosh hoyeche");
+}
+else
+{
+  console.log("biyer boyosh hoyni");
+}
+
+var age = 23;
+if (age>=21)
+{
+  console.log("Chele hole biyer boyosh hoyeche");
+}
+else if(age>=18)
+{
+  console.log("maya hole biyer boyosh hoyeche");
+}
+else
+{
+  console.log("biyer boyosh hoyni");
+}
+
+var age = 18;
+if (age>=21)
+{
+  console.log("Chele hole biyer boyosh hoyeche");
+}
+else if(age>=18)
+{
+  console.log("maya hole biyer boyosh hoyeche");
+}
+else
+{
+  console.log("biyer boyosh hoyni");
+}
+
+var age = 19;
+if (age>=21)
+{
+  console.log("Chele hole biyer boyosh hoyeche");
+}
+else if(age>=18)
+{
+  console.log("maya hole biyer boyosh hoyeche");
+}
+else
+{
+  console.log("biyer boyosh hoyni");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
