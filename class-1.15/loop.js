73f1af6e455f906ee6b5eebf6da8a2695e4b3773
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-16 00:20:48 +0600

    Create loop.js

---
+++ class-1.15/loop.js
@@ -0,0 +1,15 @@
+var a = 0;
+while(a<5){
+    console.log(a);
+    a++;
+    console.log("boom");
+}
+    console.log("mahi");
+
+    var b =0;
+    while(b<=5){
+        console.log(b);
+        b++;
+        console.log("silicon vally")
+    }
+        console.log("mahi chowdhury")
