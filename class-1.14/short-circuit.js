3db49d79800c99f44c2c60a07968f242fd75a469
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-15 20:02:58 +0600

    Create short-circuit.js

---
+++ class-1.14/short-circuit.js
@@ -0,0 +1,18 @@
+var a = true;
+if (a){
+    console.log("Hellow world");
+}
+
+
+
+var a = 4;
+
+a && console.log("hello world");
+
+var b ;
+b || (b=5);
+console.log(b);
+
+var c = 8;
+c || (c=10);
+console.log(c);
