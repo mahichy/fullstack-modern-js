93441a73ddb0ec6e2641748a39f2e6e77c038311
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-17 10:53:55 +0600

    Create not-operator.js
    complex subject

---
+++ class-1.16/not-operator.js
@@ -0,0 +1,136 @@
+if (true){
+    console.log("hello world");
+}
+else{
+    console.log("not so hello world");
+}
+
+if (false){
+    console.log("hello world");
+}
+else{
+    console.log("not so hello world");
+}
+
+if (!true){
+    console.log("hello world");
+}
+else{
+    console.log("not so hello world");
+}
+
+if (!false){
+    console.log("hello world");
+}else{
+    console.log("not so hello world")
+}
+
+if (true && true){
+    console.log("hello world");
+}else{
+    console.log("not so hello world");
+}
+
+if (!true && true){
+    console.log("hello world");
+}else{
+    console.log("not so hello world");
+}
+
+if (!true || true){
+    console.log("hello world");
+}else{
+    console.log("not so hello world");
+}
+
+if (!(true || true)){
+    console.log("hello world");
+}else{
+    console.log("not so hello world");
+}
+
+if (!true || !true){
+    console.log("hello world");
+}else{
+    console.log("not so hello world");
+}
+
+var a = "mahi";
+
+
+console.log(`hello ${a}`);
+
+var a ="";
+if (a==""){
+  console.log("a was not provided")
+}else{
+  console.log(`hello ${a}`);  
+}
+
+var a ="mahi";
+if (a==""){
+  console.log("a was not provided")
+}else{
+  console.log(`hello ${a}`);  
+}
+
+var a =undefined;
+if (a==""){
+  console.log("a was not provided")
+}else{
+  console.log(`hello ${a}`);  
+}
+
+var a =undefined;
+if (!a){
+  console.log("a was not provided")
+}else{
+  console.log(`hello ${a}`);  
+}
+
+var a ="";
+if (!a){
+  console.log("a was not provided")
+}else{
+  console.log(`hello ${a}`);  
+}
+
+console.log(!false);
+console.log(!true);
+console.log(!"");
+console.log(!0);
+console.log(!1);
+
+
+console.log(Boolean(""));
+
+
+console.log(!"");
+
+console.log(!!"");
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
