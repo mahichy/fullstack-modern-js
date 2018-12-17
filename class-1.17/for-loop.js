f0938809def4edea2fbc387475fad7e3fb18beda
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-17 11:35:19 +0600

    Create for-loop.js
    for loop its interesting

---
+++ class-1.17/for-loop.js
@@ -0,0 +1,37 @@
+let a = 0;
+while(a<2){
+    console.log(a);
+    a++;
+}
+
+
+let b = 0;
+while(b<=3){
+    console.log(b);
+    b++;
+}
+
+for(let b = 0; b<=10;b++){
+    console.log(b);
+}
+
+//b+=2,b=b+2
+for(let b = 2; b<=10;b+=2){
+    console.log(b);
+}
+
+for (let d = 10; d>0; d-=2){
+  console.log(d)
+}
+
+for (let e = 5; e<=100;e+=5){
+  console.log(e)
+}
+
+
+
+
+
+
+
+
