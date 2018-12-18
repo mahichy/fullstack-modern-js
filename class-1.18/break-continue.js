b6d438a3c52cc2dfa60c160c246ee6eebdba0ff8
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-19 00:00:25 +0600

    Update break-continue.js

--- class-1.18/break-continue.js
+++ class-1.18/break-continue.js
@@ -1,19 +1,3 @@
-/*
-// continue;
-// break;
-var found = 0;
-var target =21;
-var iterator = 2;
-
-while (!found){
-    let answer = target % iterator;
-    if (!answer){
-        found = iterator;
-    }
-    iterator++;
-}
-console.log(found);
-*/
 
 var target =21;
 var found =0;
@@ -24,3 +8,33 @@ for (let i=2;!found;i++){
     }
 }
 console.log(found);
+
+
+var target =21;
+var found =0;
+for (let i=2;i<=target/2;i++){
+    let result = target%i;
+    if (!result){
+      found =i;
+        break;
+    }
+}
+console.log(found);
+
+
+var target =21;
+var target2 =90;
+var found =0;
+for (let i=2;i<target/2;i++){
+    let result = target%i;
+    if (result){
+        continue;
+    }
+    let result2=target2%i;
+    if(!result2){
+      found=i;
+      break;
+    }
+}
+console.log(found);
+
