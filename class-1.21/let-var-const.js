ca49f281655dbd76658f1778436e06b0c44f8066
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-22 20:44:01 +0600

    Create let-var-const.js

---
+++ class-1.21/let-var-const.js
@@ -0,0 +1,14 @@
+var a = "this is a";
+let b = "this is b";
+const c = "this is c";
+console.log()
+// global scope
+{
+  console.log(a);
+  var d = " this is d";
+  console.log(d);
+  let e = " this is e";
+  console.log(e);
+} 
+//console.log(e);
+console.log(d);
