ce7dba68dbd3649ab419f68fc182b29242859f87
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-24 15:19:57 +0600

    Create hoeasting.js

---
+++ class-1.25/hoeasting.js
@@ -0,0 +1,20 @@
+//function declaration
+
+console.log(add (5,9));
+console.log(add2(5,9));
+
+function add (a,b){
+    return a+b;
+}
+
+function add2(a,b){
+    return a*b;
+}
+
+//function exprassion
+
+var subtract =function(a,b){
+    return a-b;
+    console.log(subtract(25,35));
+
+}
