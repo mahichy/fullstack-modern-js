c7fabb74ff67e2e027cdf5609a8cc77307a7c46e
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-24 11:21:23 +0600

    Create function-expre.js

---
+++ class-1.24/function-expre.js
@@ -0,0 +1,19 @@
+var add =function(a,b){
+    return (a+b)*2;
+}
+console.log(add(10,25));
+
+var add2 = function(c,d){
+    return c-d;
+}
+console.log(add2(85,42));
+
+var add3 = function(e,f){
+    return e*f;
+}
+console.log(add3(22,41));
+
+var add4 = function(g,h){
+    return (g/h)/2;
+}
+console.log(add4(84,21));
