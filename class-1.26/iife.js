02e86e3469368dffca6457c011523d841056028d
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-24 15:48:25 +0600

    Create iife.js

---
+++ class-1.26/iife.js
@@ -0,0 +1,17 @@
+//IIFE
+
+(function(){
+    var x = 5;
+    console.log(x);
+})()
+
+!function(){
+    var x = 5;
+    console.log(x);
+}()
+
+
++function(){
+    var x = 5;
+    console.log(x);
+}()
