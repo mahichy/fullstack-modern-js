d2d88a5c8dd07be71d615077261004853a9ba6d9
mahichy <43366576+mahichy@users.noreply.github.com>
2018-12-25 19:07:30 +0600

    Create arrow-function.js

---
+++ class-1.27/arrow-function.js
@@ -0,0 +1,30 @@
+/*
+var x = function(a){
+    return a+1;
+}
+console.log(x(4))
+console.log(x(7))
+console.log(x(8))
+console.log(x(9))
+
+
+//new( for one argument)
+var x = a=>a+1;
+
+console.log(x(4))
+console.log(x(7))
+console.log(x(8))
+console.log(x(9))
+
+*/
+//more than one argument 
+var x =( a,b)=>{
+    console.log(a);
+    console.log(b);
+    return a+b;
+};
+
+console.log(x(4,3))
+console.log(x(7,4))
+console.log(x(8,5))
+console.log(x(9,4))
