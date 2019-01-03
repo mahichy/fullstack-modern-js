d1bf3d43f4e9ec5179fbf398b7c352c4d863ef8e
mahichy <43366576+mahichy@users.noreply.github.com>
2019-01-03 10:11:12 +0600

    Create closer-purefunction.js

---
+++ class-1.28/closer-purefunction.js
@@ -0,0 +1,24 @@
+/*
+function makeFunc(){
+    var name = "Mozilla";
+    function displayName (){
+        console.log(name);
+    }
+    return displayName;
+}
+var myFunc = makeFunc();
+myFunc();
+*/
+
+
+
+function a(b,c){
+    let f = d+e;
+    e = 7;
+    return f;
+}
+let d = 9;
+let e = 2;
+console.log(a(d,e));
+console.log(d);
+console.log(e);
