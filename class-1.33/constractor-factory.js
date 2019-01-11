b11ebac196e9d288fe4027156601e3bdb97d6b67
mahichy <43366576+mahichy@users.noreply.github.com>
2019-01-11 23:07:26 +0600

    Create constractor-factory.js

---
+++ class-1.33/constractor-factory.js
@@ -0,0 +1,12 @@
+function CreateHuman(name,age,gender){
+    return{
+        name:name,
+        age:age,
+        gender:gender,
+    }
+}
+var a = CreateHuman("anam",60,"M")
+var b = CreateHuman("hasin",45,"M")
+
+console.log(a)
+console.log(b)
