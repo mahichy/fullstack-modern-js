8ecd79fd608f75ae7448012d878ab46221da8953
mahichy <43366576+mahichy@users.noreply.github.com>
2019-01-17 10:29:04 +0600

    Create math-object.js

---
+++ class-1.42/math-object.js
@@ -0,0 +1,61 @@
+console.log(Math.PI);
+console.log(Math.pow(4,2))
+
+function circleArea(r){
+    return Math.PI*Math.pow(r,2);
+}
+console.log(circleArea(4))
+
+function circle(r){
+    return 2*Math.PI*r;
+}
+console.log(circle(4))
+
+console.log(Math.LN10)
+
+let maxNum =Math.max (3,5,6,7,2,3)
+
+console.log(maxNum);
+
+let maxNum2 =Math.min (3,5,6,7,2,3)
+
+console.log(maxNum2);
+
+let myArr = [3,5,6,7,2,3]
+console.log(Math.max.apply(null,myArr));
+
+let myNum = 1.44;
+console.log(Math.floor(myNum));
+
+let myArr2 = 1.44;
+console.log(Math.ceil(myArr2));
+
+let myNum2 = 8.44;
+console.log(Math.round(myNum2));
+
+let myNum3 = 8.52;
+console.log(Math.round(myNum3));
+
+let myNum4 = -5.5;
+console.log(Math.abs(myNum4));
+
+let myNum5 = 5.5;
+console.log(Math.sin(myNum5));
+
+let myNum6 = 5.5;
+console.log(Math.cos(myNum6));
+
+let myNum7 = 5.5;
+console.log(Math.tan(myNum7));
+
+console.log(Math.random());
+
+var myRandom = Math.random()*10;
+console.log(myRandom)
+
+var myRandom2 =5+ Math.random()*10;
+console.log(Math.round(myRandom2))
+
+
+
+
